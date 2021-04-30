4b6f796e6546ebaffd58e91d971ec5769db0535a
Razei <razeixyz@gmail.com>
2021-04-30 12:07:05 -0400

    Added SVG to homepage using D3

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import * as D3 from 'd3/index';
 
 @Component({
   selector: 'app-homepage',
@@ -7,11 +8,21 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  host: any;
+  svg: any;
   constructor(private _element: ElementRef) {
-    
+    this.host = D3.select(this._element.nativeElement);
   }
 
   ngOnInit() {
+    this.buildSVG();
   }
 
+  buildSVG() {
+    this.host.html('');
+    this.svg = this.host.append('svg')
+    .attr('width', '600')
+    .attr('height', '400')
+    .style('background-color', 'blue');
+  }
 }
